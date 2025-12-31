e965948e346c12ca9dfa792eb51cd0dfb99095da
Vahid Ghafarpour <vahid@smartoire.com>
2025-12-30 20:50:37 -0800

    Update app configuration details

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,13 +1,10 @@
 export default defineAppConfig({
-  title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/miantiao-me/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
+  title: 'Oire.ca',
+  email: 'oire@smartoire.com',
+  github: 'https://github.com/Smartoire',
+  twitter: 'https://x.com/smartoire',
+  blog: 'https://cloudoire.com',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
